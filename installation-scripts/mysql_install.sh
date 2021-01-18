c9bf54fd0157807c31d93991b5819527d9135ceb
Manuel Alejandro Coral Lozano <almacolo23@hotmail.com>
2021-01-18 14:16:20 -0500

    updated mysql script

--- installation-scripts/mysql_install.sh
+++ installation-scripts/mysql_install.sh
@@ -10,4 +10,4 @@ sudo mysql < ./create_schema_user.sql
 
 echo "Populating DB"
 
-sudo mysql movie_db < Ramp-Up/movie-analyst-api/data_model/table_creation_and_inserts.sql 
+sudo mysql movie_db < ./BackEnd-DRU/movie-analyst-api/data_model/table_creation_and_inserts.sql 
